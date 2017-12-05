661e2c18acb154a295a704b80e9da3761db812b6
GWilman <georgewilman1@gmail.com>
2017-12-05 18:32:53 +0000

    picks route + secure routes tested

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,26 @@
 const router = require('express').Router();
 const auth  = require('../controllers/auth');
+const users  = require('../controllers/users');
 const teams  = require('../controllers/teams');
 const players  = require('../controllers/players');
 const leagues  = require('../controllers/leagues');
-const users  = require('../controllers/users');
-// const secureRoute = require('../lib/secureRoute');
+const picks  = require('../controllers/picks');
+// Add secure route to all applicable (currently tested on league create - works(adds createdBy))
+const secureRoute = require('../lib/secureRoute');
 
 router.route('/register')
   .post(auth.register);
 
 router.route('/login')
   .post(auth.login);
+
+router.route('/users')
+  .get(users.index);
+
+router.route('/users/:id')
+  .get(users.show)
+  .put(users.update)
+  .delete(users.delete);
 
 router.route('/teams')
   .get(teams.index);
@@ -26,20 +36,21 @@ router.route('/players/:id')
 
 router.route('/leagues')
   .get(leagues.index)
-  .post(leagues.create);
+  .post(secureRoute,leagues.create);
 
 router.route('/leagues/:id')
   .get(leagues.show)
   .put(leagues.update)
   .delete(leagues.delete);
 
-router.route('/users')
-  .get(users.index);
+router.route('/picks')
+  .get(picks.index)
+  .post(secureRoute, picks.create);
 
-router.route('/users/:id')
-  .get(users.show)
-  .put(users.update)
-  .delete(users.delete);
+router.route('/picks/:id')
+  .get(picks.show)
+  .put(picks.update)
+  .delete(picks.delete);
 
 router.route('/*')
   .all((req, res) => res.notFound());
---
+++ controllers/picks.js
@@ -0,0 +1,64 @@
+const Pick = require('../models/pick');
+
+function picksIndex(req, res, next) {
+  Pick
+    .find()
+    .populate('createdBy league champion runnerUp topScoringTeam mostYellowsTeam topScorer mostAssists mostYellows sentOff finalMoM')
+    .exec()
+    .then(picks => res.json(picks))
+    .catch(next);
+}
+
+function picksCreate(req, res, next) {
+  req.body.createdBy = req.currentUser;
+
+  Pick
+    .create(req.body)
+    .then(pick => res.status(201).json(pick))
+    .catch(next);
+}
+
+function picksShow(req, res, next) {
+  Pick
+    .findById(req.params.id)
+    .populate('createdBy league champion runnerUp topScoringTeam mostYellowsTeam topScorer mostAssists mostYellows sentOff finalMoM')
+    .exec()
+    .then((pick) => {
+      if(!pick) return res.notFound();
+      res.json(pick);
+    })
+    .catch(next);
+}
+
+function picksUpdate(req, res, next) {
+  Pick
+    .findById(req.params.id)
+    .exec()
+    .then((pick) => {
+      if(!pick) return res.notFound();
+      pick = Object.assign(pick, req.body);
+      return pick.save();
+    })
+    .then(pick => res.json(pick))
+    .catch(next);
+}
+
+function picksDelete(req, res, next) {
+  Pick
+    .findById(req.params.id)
+    .exec()
+    .then((pick) => {
+      if(!pick) return res.notFound();
+      return pick.remove();
+    })
+    .then(() => res.status(204).end())
+    .catch(next);
+}
+
+module.exports = {
+  index: picksIndex,
+  create: picksCreate,
+  show: picksShow,
+  update: picksUpdate,
+  delete: picksDelete
+};
